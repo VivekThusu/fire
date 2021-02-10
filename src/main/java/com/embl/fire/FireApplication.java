5b5c7a7fa5d9039719ad1bfb6b843351d288a468
VivekThusu <vivek.thusu04@gmail.com>
2021-02-11 02:39:25 +0530

    Update FireApplication.java

--- src/main/java/com/embl/fire/FireApplication.java
+++ src/main/java/com/embl/fire/FireApplication.java
@@ -9,7 +9,6 @@ public class FireApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FireApplication.class, args);
-        PersonDTO o = new PersonDTO();
     }
 
 }
